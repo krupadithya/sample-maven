0a18581c1832d5247b6e1314dd8b2a256e1278b3
EC2 Default User <ec2-user@ip-172-31-47-89.ap-south-1.compute.internal>
2021-05-16 14:22:46 +0000

    changes for git integration

--- src/main/java/com/devopsclass/App.java
+++ src/main/java/com/devopsclass/App.java
@@ -8,6 +8,6 @@ public class App
 {
     public static void main( String[] args )
     {
-        System.out.println( "Hello World Again, changes from CLI!");
+        System.out.println( "Hello World Again, changes from CLI!, A new change added for git jenkins automation testing");
     }
 }
